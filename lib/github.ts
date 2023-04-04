b98519869b04083d5961fdabf95dc6ede655ead9
Jonathan Hudak <hudak.jonathan@gmail.com>
2023-04-03 22:14:13 -0700

    Add redirect uri env var

--- lib/github.ts
+++ lib/github.ts
@@ -7,6 +7,7 @@ const env = await load();
 
 const GH_CLIENT_ID = env["GH_CLIENT_ID"];
 const GH_CLIENT_SECRET = env["GH_CLIENT_SECRET"];
+const GH_REDIRECT_URI = env["GH_REDIRECT_URI"];
 
 let app: typeof OAuthApp;
 export function getApp(): typeof OAuthApp {
@@ -17,7 +18,7 @@ export function getApp(): typeof OAuthApp {
     clientType: "github-app",
     clientId: GH_CLIENT_ID,
     clientSecret: GH_CLIENT_SECRET,
-    redirectUrl: "http://localhost:8000/api/login/github/oauth",
+    redirectUrl: GH_REDIRECT_URI,
   });
   return app;
 }
